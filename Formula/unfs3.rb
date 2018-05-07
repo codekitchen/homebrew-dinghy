57ec51c21fe8e9327e5f67077d55b8e5dcf40181
Marc Siegel <marc@usainnov.com>
2018-05-07 12:31:27 -0400

    Add Homebrew devel block to use UNFS3 branch 'higher-resolution-setattr'

--- Formula/unfs3.rb
+++ Formula/unfs3.rb
@@ -4,6 +4,14 @@ class Unfs3 < Formula
   url "https://downloads.sourceforge.net/project/unfs3/unfs3/0.9.22/unfs3-0.9.22.tar.gz"
   sha256 "482222cae541172c155cd5dc9c2199763a6454b0c5c0619102d8143bb19fdf1c"
 
+  devel do
+    url "https://github.com/derfian/unfs3.git", :branch => "higher-resolution-setattr"
+    version "0.9.23-pre"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   head do
     url "http://svn.code.sf.net/p/unfs3/code/trunk/"
 
@@ -13,7 +21,7 @@ class Unfs3 < Formula
 
   def install
     ENV.deparallelize # Build is not parallel-safe
-    system "./bootstrap" if build.head?
+    system "./bootstrap" if build.head? || build.devel?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
