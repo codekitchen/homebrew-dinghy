8fc853abbf5c9d45c122ee90883abf5e39b73c26
Brian Palmer <brian@codekitchen.net>
2018-07-05 15:14:30 -0600

    release v4.6.5

--- Formula/dinghy.rb
+++ Formula/dinghy.rb
@@ -1,7 +1,7 @@
 class Dinghy < Formula
   desc "faster, friendlier Docker on OS X"
   homepage "https://github.com/codekitchen/dinghy"
-  url "https://github.com/codekitchen/dinghy.git", :tag => "v4.6.4", :revision => "bed8ce2a85168f972e3576a3b8f7fb3a934ca94b"
+  url "https://github.com/codekitchen/dinghy.git", :tag => "v4.6.5", :revision => "c7c0a2e364b2ed2e61a2844caa399a22273a26f0"
   head "https://github.com/codekitchen/dinghy.git", :branch => :master
 
   depends_on "codekitchen/dinghy/unfs3"
